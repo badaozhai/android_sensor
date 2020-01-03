66d7ccb4c4aa377e86b8eb63c3fbd205778bc915
badaozhai <89066216@qq.com>
2020-01-03 13:18:40 +0800

    init

---
+++ app/src/main/java/com/chenjian/android_sensor/MainActivity.java
@@ -0,0 +1,14 @@
+package com.chenjian.android_sensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
